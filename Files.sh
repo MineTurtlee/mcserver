4bf2f2b0bc831c8cf38106114ddfdaa9832083cc
MineTurtlee <ghastboynguyen1534@gmail.com>
2024-12-30 14:47:45 +0700

    Update Files.sh

--- Files.sh
+++ Files.sh
@@ -1,3 +1,13 @@
+Green_font_prefix="\033[32m"
+Red_font_prefix="\033[31m"
+Green_background_prefix="\033[42;37m"
+Red_background_prefix="\033[41;37m"
+Font_color_suffix="\033[0m"
+INFO="[${Green_font_prefix}INFO${Font_color_suffix}]"
+ERROR="[${Red_font_prefix}ERROR${Font_color_suffix}]"
+LOG_FILE='/tmp/ngrok.log'
+TELEGRAM_LOG="/tmp/telegram.log"
+CONTINUE_FILE="/tmp/continue"
 ERRORS_LOG=$(grep "command failed" ${LOG_FILE})
 
 if [[ -e "${LOG_FILE}" && -z "${ERRORS_LOG}" ]]; then
