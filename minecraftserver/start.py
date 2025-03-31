5bb18ddf5b6a00b16e44d2ec9575a823e2886d58
MineTurtlee <ghastboynguyen1534@gmail.com>
2025-03-31 18:49:37 +0700

    Update start.py

--- minecraftserver/start.py
+++ minecraftserver/start.py
@@ -13,6 +13,7 @@ async def server():
             stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False
         )
         return process
+        print("Minecraft:" + stdout)
     except Exception as e:
         logging.error(f"Failed to start server process: {e}")
         return None
@@ -25,6 +26,7 @@ async def proxy():
         )
         await asyncio.sleep(5)  # Give ngrok time to initialize
         return process
+        print("NGROK:" + stdout)
     except Exception as e:
         logging.error(f"Failed to start proxy process: {e}")
         return None
