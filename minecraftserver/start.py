74008f1df1b5e525f15755d3ef1f0edc33e28333
MineTurtlee <ghastboynguyen1534@gmail.com>
2025-03-31 16:05:38 +0700

    Update start.py

--- minecraftserver/start.py
+++ minecraftserver/start.py
@@ -10,10 +10,27 @@ async def server():
 # Proxy Async
 
 async def proxy():
-  subprocess.call("ngrok http --url=sharply-sought-chipmunk.ngrok-free.app 80", shell=True)
+  subprocess.call("ngrok http --url=sharply-sought-chipmunk.ngrok-free.app 25565", shell=True)
 
 # Bot Async
 
 async def bot():
-  import discord.py
+  import discord
+  from   discord import errors
+  import os
+  import certifi
+  
+  token = os.getenv('Bot_Token')
+  channelid= os.getenv('Channel_ID')
+
+  os.environ["SSL_CERT_FILE"] = certifi.where()
+
+  client = discord.Client(intents=discord.Intents.default(),activity=discord.Activity(name='The Minecraft server status', type=discord.ActivityType.listening))
+
+  async def on_ready(self):
+    print(f'Logged in as {self.user} (ID: {self.user.id})')
+    asyncio.sleep(120)
+    print("Sending started message...")
+    channel = client.get_channel(channelid)
+    await channel.send("The server is probably up at https://sharply-sought-chipmunk.ngrok-free.app!")
   
